90322121e51a1ffa5684a1da94bf6f37a690d915
RAFOUACTIF <bgmgaming09@gmail.com>
2025-07-26 18:06:06 +0200

    docker chilled

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-# Lancer le serveur web Gunicorn en arrière-plan
 echo "--- Démarrage du serveur web Flask/Gunicorn... ---"
-
-# CORRECTION : Utilisation de workers 'gevent' plus performants pour les appels API
-gunicorn --workers 3 --worker-class gevent --bind 0.0.0.0:10000 --timeout 60 app:app &
+# ON PASSE DE 3 WORKERS À 1 POUR ÉCONOMISER LA RAM
+gunicorn --workers 1 --worker-class gevent --bind 0.0.0.0:10000 --timeout 60 app:app &
 
 echo "--- Démarrage du Bot Discord au premier plan... ---"
 python3 bot_runner.py
