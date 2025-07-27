f467d61ed237274e747bb2340e0c5fe2be6e7ea2
RAFOUACTIF <bgmgaming09@gmail.com>
2025-07-27 21:37:54 +0200

    oui

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#!/bin/bash
-
-echo "--- Démarrage du Bot Discord en arrière-plan... ---"
-python3 bot_runner.py
-
-echo "--- Démarrage du serveur web Flask/Gunicorn au premier plan... ---"
-# Gunicorn est maintenant le processus principal. Le conteneur restera en vie tant qu'il tourne.
-gunicorn --workers 1 --bind 0.0.0.0:10000 --timeout 60 app:app
+echo "--- Démarrage du Bot Discord... ---"
+# On ne lance QUE le bot. L'API est gérée par docker-compose.
+python bot_runner.py
