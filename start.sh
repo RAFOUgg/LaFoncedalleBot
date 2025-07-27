4d36bf045792956cd2439a170e5be1924301b635
RAFOUACTIF <bgmgaming09@gmail.com>
2025-07-27 19:59:40 +0200

    gunicorn start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "--- Démarrage du serveur web Flask/Gunicorn... ---"
 # ON PASSE DE 3 WORKERS À 1 POUR ÉCONOMISER LA RAM
-gunicorn --workers 1 --worker-class gevent --bind 0.0.0.0:10000 --timeout 60 app:app &
+gunicorn --workers 1 --bind 0.0.0.0:10000 --timeout 60 app:app &
 
 echo "--- Démarrage du Bot Discord au premier plan... ---"
 python3 bot_runner.py
