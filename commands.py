a1a6360b4a0dfde0bd22a6443197266107075691
RAFOUACTIF <bgmgaming09@gmail.com>
2025-07-28 02:37:47 +0200

    test

--- commands.py
+++ commands.py
@@ -213,7 +213,7 @@ class RatingsPaginatorView(discord.ui.View):
         embed.add_field(name="Description du Produit", value=p_details.get('detailed_description', 'N/A')[:1024], inline=True)
         embed.add_field(name="Prix", value=p_details.get('price', 'N/A'), inline=True)
         embed.add_field(name="Note de la Communauté", value=community_score_str, inline=False)
-        embed.add_field(name="Votre Note Globale", value=f"**{user_avg:.2f} / 10**", inline=True)
+        embed.add_field(name="Votre Note Globale", value=f"**{user_avg:.2f} / 10**", inline=False)
 
         notes = (f"👀 Visuel: `{rating.get('visual_score', 'N/A')}`\n👃 Odeur: `{rating.get('smell_score', 'N/A')}`\n"
                  f"🤏 Toucher: `{rating.get('touch_score', 'N/A')}`\n👅 Goût: `{rating.get('taste_score', 'N/A')}`\n"
