066c869dd02393b809bbcadae65c3f41179ddeed
RAFOUACTIF <bgmgaming09@gmail.com>
2025-07-28 16:25:47 +0200

    sleep API

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
+# Lance l'API Flask (gunicorn) en arrière-plan
+echo "--- Démarrage de l'API Flask (Gunicorn)... ---"
+gunicorn --bind 0.0.0.0:10000 app:app &
+
+# Attend une seconde pour être sûr que l'API a démarré
+sleep 1
+
+# Lance le Bot Discord en avant-plan
 echo "--- Démarrage du Bot Discord... ---"
-# On ne lance QUE le bot. L'API est gérée par docker-compose.
 python bot_runner.py
