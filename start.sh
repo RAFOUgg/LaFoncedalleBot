a81665ce81385b3ccac14ac4e9c79ac6da265a04
RAFOUACTIF <bgmgaming09@gmail.com>
2025-07-29 23:08:50 +0200

    start.sh gunicorn add

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Lance l'API Flask (gunicorn) en arrière-plan
-echo "--- Démarrage de l'API Flask (Gunicorn)... ---"
-gunicorn --bind 127.0.0.1:10000 app:app &
-
-# Attend une seconde pour être sûr que l'API a démarré
-sleep 1
-
-# Lance le Bot Discord en avant-plan
+# Ce script est maintenant uniquement pour le conteneur du bot.
 echo "--- Démarrage du Bot Discord... ---"
 python bot_runner.py
